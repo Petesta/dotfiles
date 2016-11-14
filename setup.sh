9410ed1f23c8423faad24a20c132cee27aa14c7e
Petesta <Petesta@live.com>
2016-11-13 23:04:28 -0500

    Reordering of statements.

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 
 readonly DIR=$(dirname $0)
 
-declare -a files
-
 if [[ $DIR != '.' ]]; then
   echo 'ERR: Setup needs to be run from within the dotfiles directory, exiting...'
   exit 1
 fi
 
 echo 'Dotfiles setup starting.'
+
+declare -a files
 
 files+=($(find . -name '.*' -type f -exec basename {} ';'))
 
