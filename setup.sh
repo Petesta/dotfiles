27b9b256bb5e17c216849dc836d5793209a769a0
Pete Cruz <iPetesta@gmail.com>
2016-11-23 17:07:29 -0800

    Add color to output for setup.sh.

--- setup.sh
+++ setup.sh
@@ -2,27 +2,44 @@
 
 readonly DIR=$(dirname $0)
 
-if [[ $DIR != '.' ]]; then
-  echo 'ERR: Setup needs to be run from within the dotfiles directory, exiting...'
-  exit 1
+declare -r colors="$DIR/.colors"
+declare -r functions="$DIR/.functions"
+
+if [[ -f $colors && -f $functions ]]; then
+  . $colors
+  . $functions
 fi
 
-echo 'Dotfiles setup starting.'
+if [[ $DIR != '.' && ! -f $colors || ! -f $functions ]]; then
+  printf  'ERR: Setup needs to be run from withing the dotfiles directory'
+  exit 1
+elif [[ $DIR != '.' ]]; then
+  p_error 'ERR: Setup needs to be run from within the dotfiles directory, exiting...'
+  exit 1
+else
+  p_note 'Dotfiles setup starting.'
 
-declare -a files
+  declare -a files=($(find . -name '.*' -type f -exec basename {} ';'))
 
-files+=($(find . -name '.*' -type f -exec basename {} ';'))
+  for file in "${files[@]}"; do
+    ln -sf "$DIR/$file" "$HOME/$file"
+    if [ $? -eq 0 ]; then
+      p_success "Symlinking $file"
+    else
+      p_error "ERR: symlinking $file"
+    fi
+  done; unset file
 
-for file in "${files[@]}"; do
-  ln -sf "$DIR/$file" "$HOME/$file"
-done; unset file
+  p_note 'Dotfiles setup complete.'
 
-echo 'Dotfiles setup complete.'
+  declare -r bashrc="$HOME/.bashrc"
 
-declare -r bashrc="$HOME/.bashrc"
+  p_note "Now sourcing $bashrc"
 
-echo "Now sourcing $bashrc"
-
-. "$bashrc"
-
-echo "Done sourcing $bashrc"
+  . "$bashrc"
+  if [ $? -eq 0 ]; then
+    p_success "Done sourcing $bashrc"
+  else
+    p_error "ERR: Problem sourcing $bashrc"
+  fi
+fi
